7819c9e9b1c48d42ea63c7a7de239fe26dfa853b
SeM2x <houssem135.302@gmail.com>
2023-10-18 02:20:51 +0100

    bug fix

--- parse_argument_width.c
+++ parse_argument_width.c
@@ -4,59 +4,63 @@
  * parse_argument - extract width and height from template
  * Return: a WIDTH struct
  */
-#define length_modifiers "hl"
-#define flags "-+ #0"
+char length_modifiers[] = "hl";
+char flags[]  = "-+ #0";
+int temp_num_flags = 1024;
 
 WIDTH *parse_argument(char *template)
 {
-	WIDTH *result;
-	int i, length;
-	char *field;
-	char *fractional;
+    WIDTH *result;
+    int i, length;
+    char *field;
+    char *fractional;
 
-	field	= malloc(sizeof(char) * 64);
-	fractional = malloc(sizeof(char) * 64);
+    field	= malloc(sizeof(char) * 64);
+    fractional = malloc(sizeof(char) * 64);
 
-	result = malloc(sizeof(WIDTH));
-	result->field = 0;
-	result->fractional = 0;
+    result = malloc(sizeof(WIDTH));
+    result->field = 0;
+    result->fractional = 0;
+    result->flags = malloc(sizeof(char) * temp_num_flags);
 
-	length = 0; /* length is to count chars in %[here]f */
+    length = 0; /* length is to count chars in %[here]f */
 
-	/* extracting the flag */
-	if (str_contains(flags, *template))
-	{
-		result->flag = *template;
-		length++;
-		template++;
-	}
+    /* extracting the flag */
+    while (str_contains(flags, *template))
+    {
+        result->flags[length] = *template;
+        length++;
+        template++;
+    }
 
-	for (i = 0; *template != '.' && (is_digit(*template) || str_contains(length_modifiers, *template)); template++)
-	{
-		field[i] = *template;
-		i++;
-		length++;
-	}
-	field[i] = 0;
+    result->flags[length] = '\0'; /*seal the thing once and for all!*/
 
-	template++;
+    for (i = 0; *template != '.' && (is_digit(*template) || str_contains(length_modifiers, *template)); template++)
+    {
+        field[i] = *template;
+        i++;
+        length++;
+    }
+    field[i] = 0;
 
-	for (i = 0; is_digit(*template); template++)
-	{
-		fractional[i] = *template;
-		i++;
-		length++;
-	}
+    template++;
 
-	fractional[i] = 0;
+    for (i = 0; is_digit(*template); template++)
+    {
+        fractional[i] = *template;
+        i++;
+        length++;
+    }
 
-	if (str_contains(length_modifiers, *template))
-		result->modifier = *template;
+    fractional[i] = 0;
+
+    if (str_contains(length_modifiers, *template))
+        result->modifier = *template;
 
 
-	result->field = str_to_int(field);
-	result->fractional = str_to_int(fractional);
-	result->length = length + (result->fractional ? 1 : 0) ;
+    result->field = str_to_int(field);
+    result->fractional = str_to_int(fractional);
+    result->length = length + (result->fractional? 1:0) ;
 
-	return (result);
+    return (result);
 }
