023bdb00643fce7daf4667f4a20f523f5ca2c5a2
kiskiller0 <kiskiller0@gmail.com>
2023-10-18 01:18:03 +0100

    print_reverse

--- test/print_reverse.c
+++ test/print_reverse.c
@@ -1,3 +1,16 @@
 #include "main.h"
 
 
+/**
+ * print_reverse - does what it says
+ * @str: asdasd
+ */
+
+void print_reverse(char *str)
+{
+	if (*str)
+	{
+		print_reverse(str + 1);
+		putchar(*str);
+	}
+}
